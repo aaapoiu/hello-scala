efcdd7f3809e67fc7821a649fb9bc234e96d7707
hyo <leehj08@coupang.com>
2016-12-04 22:35:00 +0900

    Generic

--- IntegerReference.scala
+++ IntegerReference.scala
@@ -9,7 +9,7 @@ class Reference[T] {
 object IntegerReference {
     def main(args: Array[String]) {
         val cell = new Reference[Int]
-        cell.set(13)
+        cell.set(13) // 정수 외에 다른 것은 넣을 수 없다
         println("Reference contains the half of " + cell.get * 2)
     }
 }
